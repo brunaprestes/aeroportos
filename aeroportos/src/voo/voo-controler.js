ce316c562834ff863adfcd0767874400a918cb18
Bruna Ferreira Fernandes <brunaff1996@hotmail.com>
2019-03-23 01:42:51 -0300

    get options values

--- aeroportos/src/voo/voo-controler.js
+++ aeroportos/src/voo/voo-controler.js
@@ -3,41 +3,47 @@ import axios from 'axios';
 import Voo from './voo';
 
 class VooControler extends Component {
+  partida = '';
+  destino = '';
+
   constructor(props) {
     super(props);
-
     this.state = {
       aeroportos: [],
       voos: [],
+      partida: '',
+      destino: '',
     };
+    this.handleChangeGo = this.handleChangeGo.bind(this);
+    this.handleChangeArrived = this.handleChangeArrived.bind(this);
   }
+
   componentDidMount() {
     axios.get('https://api-voadora.dev.tegra.com.br/flight/companies')
       .then((response) => {
-        debugger;
+        
         this.setState({
           aeroportos: response.data,
         });
         console.log(response.data);
       });
+    }
 
-       axios.post('https://api-voadora.dev.tegra.com.br/flight')
-         .then((response) => {
-           debugger;
-           this.setState({
-             voos: response.data,
-           });
-           console.log(response.data);
-         });
 
-    this.getFiltredList();
+  fligthFiltred() {
+    axios.post('https://api-voadora.dev.tegra.com.br/flight')
+      .then((response) => {
+        this.setState({
+          voos: response.data[0],
+        });
+        console.log(response.data);
+      });
   }
-
 
   getFiltredList() {
     axios.post('https://api-voadora.dev.tegra.com.br/flight')
     .then((response) => {
-      debugger;
+      
       this.setState({
         voos: response.data,
       });
@@ -49,8 +55,19 @@ class VooControler extends Component {
     return true;
   }
 
+  handleChangeGo(event) {
+    this.partida = event.target.value;
+    this.setState({partida: this.partida});
+  }
+
+  handleChangeArrived(event) {
+    this.destino = event.target.value;
+    console.log(this.destino);
+    this.setState({destino: this.destino});
+  }
+
   render() {
-    return ( <Voo aeroportos={this.state.aeroportos} > </Voo>
+    return ( <Voo aeroportos={this.state.aeroportos} handleChangeGo={this.handleChangeGo} handleChangeArrived={this.handleChangeArrived}  partida={this.partida} destino={this.destino}> </Voo>
     );
   }
 }
--- aeroportos/src/voo/voo.js
+++ aeroportos/src/voo/voo.js
@@ -4,19 +4,22 @@ import PropTypes from 'prop-types';
 import './voo.scss';
 
 const Voo = (props) => {
-  console.log(props);
-  debugger;
-  return(<div className="voo-component">
+  console.log(props); 
+  let filter;
+  
+  
+  return(
+  <div className="voo-component">
     <h1>Voôs disponíveis</h1>
     <div className="filters">
       <form>
         <label>
           Partida:
           <div className="input-group" >
-            <select className="form-control" >
+            <select value={props.partida} onChange={props.handleChangeGo} className="form-control" >
               <option value="">Selecione um aeroporto</option>
              {
-               props.aeroportos.map((aeroporto) => <option value={aeroporto.aeroporto}>{aeroporto.nome}</option>)
+               props.aeroportos.map((aeroporto) => <option value={aeroporto.aeroporto} key={`${aeroporto.aeroporto}Partida`}>{aeroporto.nome}</option>)
              }
             </select>
             <div className="input-group-append">
@@ -27,10 +30,10 @@ const Voo = (props) => {
         <label>
           Destino:
           <div className="input-group" >
-            <select className="form-control" >
+            <select value={props.destino} onChange={props.handleChangeArrived} className="form-control" >
               <option value="">Selecione um aeroporto</option>
              {
-               props.aeroportos.map((aeroporto) => <option value={aeroporto.aeroporto}>{aeroporto.nome}</option>)
+               props.aeroportos.map((aeroporto) => <option value={aeroporto.aeroporto} key={`${aeroporto.aeroporto}Destino`}>{aeroporto.nome}</option>)
              }
             </select>
             <div className="input-group-append">
@@ -41,13 +44,13 @@ const Voo = (props) => {
         <label>
           Data de embarque:
           <div className="input-group" >
-            <input className="form-control" type="date" readonly />
+            <input className="form-control" type="date" />
             <div className="input-group-append">
               <span className="input-group-text -pink"><i className="icon icon-date"></i></span>
             </div>
           </div>
         </label>
-        <input type="submit" value="Submit" className="btn btn-submit"/>
+        <button className="btn btn-submit">Enviar</button>
       </form>
     </div>
     <div className="grid-voos">
@@ -75,11 +78,12 @@ const Voo = (props) => {
 }
 
 Voo.defaultProps = {
-  aeroportos: null,
+  aeroportos: [],
 };
 
 Voo.propTypes = {
-  aeroportos: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  aeroportos: PropTypes.array,
+  filter: PropTypes.func,
 };
 
 export default Voo;
